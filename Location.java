4dcd49679b9d41608a04dbba84649edb099ec093
MarkShen1030 <56050087+MarkShen1030@users.noreply.github.com>
2019-12-16 13:39:03 -0800

    Chomp Assignment
    Chomp

---
+++ Location.java
@@ -0,0 +1,22 @@
+package chomp4students2019;
+
+public class Location
+{
+  private int row, col;
+
+  public Location(int r, int c)
+  {
+    row = r;
+    col = c;
+  }
+
+  public int getRow()
+  {
+    return row;
+  }
+
+  public int getCol()
+  {
+    return col;
+  }
+}
---
+++ Strategy.java
@@ -0,0 +1,11 @@
+package chomp4students2019;
+
+/*
+ * Chomp strategy
+ */
+
+public interface Strategy
+{
+  Location findBestMove(ChompGame game);
+  Location findRandomMove(ChompGame game);
+}
